2d5493a2f601dcb91d535777644845bbba9b15b6
AlexAlvrzCastell <39219220+AlexAlvrzCastell@users.noreply.github.com>
2018-05-20 10:14:31 -0500

    Actualización de la clase
    Se cambia atributo nombre (tipo Name) a (tipo String)
    Se cambia toString, los string no aceptan enteros como tal, toma su valor y lo transforma en ascii. Se añade la función sprintf para dicha acción

--- teaching.cpp
+++ teaching.cpp
@@ -1,69 +1,61 @@
 #include "teaching.h"
 
 ///Constructor Base
-Teaching::Teaching()
-{
+Teaching::Teaching() {
     hours = 0;
-}
+    }
 
 ///Constructor Copia
-Teaching::Teaching(const Teaching& e) : name(e.name), hours(e.hours){ }
+Teaching::Teaching(const Teaching& e) : name(e.name), hours(e.hours) { }
 
 ///Constructor Parametrizado
-Teaching::Teaching(const Name& n, Date& i, Date& f, int& h) : name(n), initialDate(i), finishDate(f), hours(h) { }
+Teaching::Teaching(const std::string& n, Date& i, Date& f, int& h) : name(n), initialDate(i), finishDate(f), hours(h) { }
 
-Name Teaching::getName()
-{
+std::string Teaching::getName() {
     return name;
-}
+    }
 
-Date Teaching::getInitialDate()
-{
+Date Teaching::getInitialDate() {
     return initialDate;
-}
+    }
 
-Date Teaching::getFinishDate()
-{
+Date Teaching::getFinishDate() {
     return finishDate;
-}
+    }
 
-int Teaching::getHours()
-{
+int Teaching::getHours() {
     return hours;
-}
+    }
 
-void Teaching::setName(const Name& n)
-{
+void Teaching::setName(const std::string& n) {
     name = n;
-}
+    }
 
-void Teaching::setInitialDate(const Date& i)
-{
+void Teaching::setInitialDate(const Date& i) {
     initialDate = i;
-}
+    }
 
-void Teaching::setFinishDate(const Date& f)
-{
+void Teaching::setFinishDate(const Date& f) {
     finishDate = f;
-}
+    }
 
-void Teaching::setHours(const int& h)
-{
+void Teaching::setHours(const int& h) {
     hours = h;
-}
+    }
 
-std::string Teaching::toString()
-{
+std::string Teaching::toString() {
     std::string result;
+    char x[5];
 
     result = "Nombre: ";
-    result += name.toString();
+    result += name;
     result += "Fecha de Inicio: ";
     result += initialDate.toString();
     result += "Fecha de Termino: ";
     result += finishDate.toString();
     result += "Cantidad de Horas Semanales: ";
-    result += hours;
+    sprintf(x, "%02i", hours);
+    result += x;
 
     return result;
-}
+    }
